f7558b43c872489141971f29824fc80dc6501167
Andrii Sakhno <andreylion.zt@gmail.com>
2024-02-10 11:34:40 +0100

    Home, Description Sections

--- Restaurant.DataAccess/Repository/Repository.cs
+++ Restaurant.DataAccess/Repository/Repository.cs
@@ -28,10 +28,18 @@ namespace Restaurant.DataAccess.Repository
 			dbSet.Add(entity);
 		}
 
-		public IEnumerable<T> GetAll(string? includeProperties = null)
+		public IEnumerable<T> GetAll(
+			Expression<Func<T, bool>>? filter = null,
+			Func<IQueryable<T>, IOrderedQueryable<T>>? orderBy = null,
+			string? includeProperties = null
+			)
 		{
 			IQueryable<T> query = dbSet;
-			if(includeProperties != null)
+			if (filter != null)
+			{
+				query = query.Where(filter);
+			}
+			if (includeProperties != null)
 			{
 				foreach (var includeProperty in includeProperties.Split(
 					new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries))
@@ -39,17 +47,33 @@ namespace Restaurant.DataAccess.Repository
 					query = query.Include(includeProperty);
 				}
 			}
+			if (orderBy != null)
+			{
+				return orderBy(query).ToList();
+			}
 
 			return query.ToList();
 		}
 
-		public T GetFirstOrDefault(Expression<Func<T, bool>>? filter = null)
+		public T GetFirstOrDefault(
+			Expression<Func<T, bool>>? filter = null, 
+			string? includeProperties = null
+			)
 		{
 			IQueryable<T> query = dbSet;
 
 			if(filter != null)
 			{
 				query = query.Where(filter);
+			}
+
+			if (includeProperties != null)
+			{
+				foreach (var includeProperty in includeProperties.Split(
+					new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries))
+				{
+					query = query.Include(includeProperty);
+				}
 			}
 
 			return query.FirstOrDefault();
---
+++ RestaurantWebApp/Pages/Customer/Home/Details.cshtml.cs
@@ -0,0 +1,26 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Restaurant.DataAccess.Repository.IRepository;
+using Restaurant.Models;
+using System.ComponentModel.DataAnnotations;
+
+namespace RestaurantWebApp.Pages.Customer.Home
+{
+    public class DetailsModel : PageModel
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        public DetailsModel(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public MenuItem MenuItem { get; set; }
+        [Range(1, 100, ErrorMessage = "Please select a count between 1 and 100")]
+        public int Count { get; set; }
+
+        public void OnGet(int id)
+        {
+            MenuItem = _unitOfWork.MenuItem.GetFirstOrDefault(u => u.Id == id, includeProperties: "Category,FoodType");
+        }
+    }
+}
